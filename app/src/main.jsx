eb2ed2a23d22cb33e57e715dfe37859b35bba14f
Manoj Gowda <84260822+ManojGowda89@users.noreply.github.com>
2025-07-13 19:28:57 +0530

    difh

---
+++ app/src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import App from "./App";
+import { CreateRouter } from "../../mjs/test";
+
+export default CreateRouter([
+      {
+    path: "/",
+    element: <App />,
+  },
+]);
+     
---
+++ mjs/test.jsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+export function CreateRouter(routes) {
+  const AppRouter = () => {
+    const router = createBrowserRouter(routes);
+    return <RouterProvider router={router} />;
+  };
+
+  return AppRouter; 
+}
